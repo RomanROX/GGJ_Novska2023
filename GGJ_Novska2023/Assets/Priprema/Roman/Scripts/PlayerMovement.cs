eb6c147331aaa91726b451250cfa140ec00c39b9
RomanROX <roman.rox.hr@gmail.com>
2023-02-01 22:43:25 +0100

    added Movementscripts
    nedovršeno

---
+++ GGJ_Novska2023/Assets/Priprema/Roman/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float PlayerSpeed;
+    public float WalkSpeed;
+    public float RunSpeed;
+    public bool IsRunning;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
